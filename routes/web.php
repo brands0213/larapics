a0e82b05f46a7cdbb182ff4bd3c229ae7cd54363
brands0213 <90987267+brands0213@users.noreply.github.com>
2022-06-20 16:21:13 +0800

    Authorizing images resource

--- routes/web.php
+++ routes/web.php
@@ -6,24 +6,9 @@ use App\Http\Controllers\ImageController;
 use App\Http\Controllers\ListImageController;
 use App\Http\Controllers\ShowImageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', ListImageController::class)->name('images.index');
+Route::get('/', ListImageController::class)->name('images.all');
 Route::get('/images/{image}', ShowImageController::class)->name('images.show');
-Route::get('/images', [ImageController::class, 'create'])->name('images.create');
-Route::post('/images', [ImageController::class, 'store'])->name('images.store');
-Route::get('/images/{image}/edit', [ImageController::class, 'edit'])->name('images.edit'); //->can('update', 'image');
-Route::put('/images/{image}', [ImageController::class, 'update'])->name('images.update');
-Route::delete('/images/{image}', [ImageController::class, 'destroy'])->name('images.destroy');
+Route::resource('/account/images', ImageController::class)->except('show');
 
 Route::view('/test-blade', 'test');
 Auth::routes();
